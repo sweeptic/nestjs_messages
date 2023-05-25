7e92f744a02d82e63ca42a2296df879097cabc41
Attila Horvath <sweeptic@gmail.com>
2023-05-25 10:15:46 +0200

    implementing a service

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,24 @@
+import { MessagesRepository } from "./messages.repository";
+
+
+export class MessagesService {
+    messageRepo: MessagesRepository
+
+    constructor() {
+        // Service is creating its own depedencies
+        // DONT DO THIS ON REAL APPS
+        this.messageRepo = new MessagesRepository()
+    }
+
+    findOne(id: string) {
+        return this.messageRepo.findOne(id)
+    }
+
+    findAll() {
+        return this.messageRepo.findAll()
+    }
+
+    create(content: string) {
+        return this.messageRepo.create(content)
+    }
+}
