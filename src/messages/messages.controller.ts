9f59ded09e205327f5538a3d09d4329b803ab5d2
Attila Horvath <sweeptic@gmail.com>
2023-05-25 11:00:56 +0200

    reporting errors in exceptions

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -23,7 +23,13 @@ export class MessagesController {
     }
 
     @Get('/:id')
-    getMessages(@Param('id') id: string) {
-        return this.messagesService.findOne(id)
+    async getMessages(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id)
+
+        if (!message) {
+            throw new NotFoundException('message not found')
+        }
+
+        return message
     }
 }
